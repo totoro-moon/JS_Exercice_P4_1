98a7d57dd01a025dd93fa752aae64e44b89b8949
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-13 13:21:37 +0200

    JS_Exercice_P4_1

---
+++ main.js
@@ -0,0 +1,16 @@
+//fonction d'un tableau activé au clic du bouton dans le html (onclick)
+//le tableau contient des données entre crochets 
+function showArray() {
+   var tableau = ['PHP', 'JavaScript', 'HTML', 'CSS', 'JAVA', 'Bootstrap'];
+   console.log(tableau);
+   // sort pour mettre dans l'ordre alphabetique
+   alert(tableau.sort());
+   // afficher le premiere element du tableau dans la console
+   console.log(tableau[0]);
+}
+
+
+
+
+
+
